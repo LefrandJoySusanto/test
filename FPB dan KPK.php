baaa20e1610d3bab2ff70be1db3e58f2782e2b97
LefrandJoySusanto <108160526+LefrandJoySusanto@users.noreply.github.com>
2022-06-25 09:51:45 +0700

    Add files via upload

---
+++ FPB dan KPK.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p><h1>FPB dan KPK</h1></p>
+<p>FPB atau faktor persekutuan terbesar adalah faktor terbesar dari dua atau lebih bilangan bulat yang membagi bilangan bulat tersebut.<br>
+Untuk mencari FPB pertama kita harus mengetahui faktor dari bilangan-bilangan yang dicari FPBnya.<br></p>
+<p>Sebagai contoh mari kita cari FBP dari bilangan 42 dan 18.</p>
+<p>Langkah pertama: cari faktor dari 42.<br>
+Faktor 42: 1, 2, 3, 6, 7, 14, 21, 42</p>
+<p>Langkah kedua: cari faktor dari 18.<br>
+Faktor 18: 1, 2, 3, 6, 9, 18</p>
+<p>Langkah ketiga: pilih faktor yang sama dan terbesar dari 42 dan 18.<br>
+Faktor 42: 1, 2, 3, 6, 7, 14, 21, 42<br>
+Faktor 18: 1, 2, 3, 6, 9, 18<br>
+Faktor yang sama dan terbesar adalah 6.</p>
+<p>Langkah keempat: ambil kesimpulan.<br>
+Jadi FPB dari 42 dan 18 adalah 6.</p>
+
+<p> test </p>
+
+</body>
+</html>
